6bba7b6fc841a16f513601decd862083968e7353
admin <admin@DESKTOP-L61T1KU>
2022-02-07 12:07:44 +0530

    Example1 project

---
+++ 02-07-2022/src/com/javaliterals/Example1.java
@@ -0,0 +1,49 @@
+package com.javaliterals;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		//write a java program to print student information using java variables and literals
+		 //variable declaration
+		int studentId;
+		String studentName;
+		int studentAge;
+		float studentMarks;
+		char studentGrade;
+		boolean studentResult;
+		String studentAddress;
+		
+		//variable initialization
+		studentId = 2022;
+		studentName = "Pallavi";
+		studentAge = 24;
+		studentMarks = 58.45f;
+		studentGrade = 'A';
+		studentResult = true;
+		studentAddress = "Hyderabad";
+		
+		System.out.print("student Id: ");
+		System.out.println(studentId);
+		
+		System.out.print("student Name: ");
+		System.out.println(studentName);
+		
+		System.out.print("student Age: ");
+		System.out.println(studentAge);
+		
+		System.out.print("student Marks: ");
+		System.out.println(studentMarks);
+		
+		System.out.print("student Grade: ");
+		System.out.println(studentGrade);
+		
+		System.out.print("student Result: ");
+		System.out.println(studentResult);
+		
+		System.out.print("student Address: ");
+		System.out.println(studentAddress);
+		
+
+	}
+
+}
